d2eb300ae7a2b871b018c7ba66386e680468ace8
George <30480991+gdowling@users.noreply.github.com>
2018-11-18 23:38:07 -0500

    Update Coordinate_Scraper.py
    Added option for list and csv data

--- Coordinate_Scraper.py
+++ Coordinate_Scraper.py
@@ -5,6 +5,7 @@ Created on Sun Nov 18 16:13:16 2018
 @author: George
 """
 import pandas as pd
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,12 +13,20 @@ Locations_file = 'Locations _to_lookup.xlsx'
 Locations_Sheet = 'Sheet1'
 Col_name = 'Locations'
 
-
-locations_df = pd.read_excel(Locations_file, sheetname=Locations_Sheet)
+try:
+    if Locations_file.split(".")[1] == 'xlsx':
+        locations_df = pd.read_excel(Locations_file, sheetname=Locations_Sheet)
+        locations_array = np.asarray(locations_df[Col_name])
+    elif Locations_file.split(".")[1] == 'csv':
+        locations_df = pd.read_csv(Locations_file)
+        locations_array = locations_df[Col_name]
+except:
+    locations_array = np.asarray(Locations_file)
+    
 features = ['Location', 'Latitude', 'Longitude']
 Complete_df = pd.DataFrame(columns=features)
     
-for loc in locations_df[Col_name]:
+for loc in locations_array:
     desired_location = loc
     search_url = 'https://www.google.com/search?q='
     url = search_url + str(desired_location.replace(' ','+'))
@@ -41,3 +50,4 @@ for loc in locations_df[Col_name]:
     
 Complete_df.to_csv('Locations_Latitude_Longitude.csv')
            
+
